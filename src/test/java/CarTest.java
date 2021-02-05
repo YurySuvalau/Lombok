67cea2c87138db5f008f6c39da01aa147cad1b7b
YurySuvalau <yury.suvalau@gmail.com>
2021-02-05 21:30:56 +0300

    Test

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,6 +1,8 @@
+import lombok.extern.log4j.Log4j2;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+@Log4j2
 public class CarTest {
 
     @Test
@@ -11,19 +13,25 @@ public class CarTest {
                 .version("diesel")
                 .build();
         Car car2 = Car.builder()
-                .speed(500)
-                .model("MTZ")
-                .version("T1000")
+                .speed(300)
+                .model("M3")
+                .version("diesel")
                 .build();
-        Car car3 = Car.builder()
-                .speed(58)
-                .model("MTZ")
-                .version("T800")
-                .build();
+//        Car car3 = Car.builder()
+//                .speed(58)
+//                .model("MTZ")
+//                .version("T800")
+//                .build();
         System.out.println(car1);
         System.out.println(car2);
-        System.out.println(car3);
-        Assert.assertEquals(car1,car2);
+       // System.out.println(car3);
+        log.fatal("fatal");
+        log.error("error");
+        log.warn("wart");
+        log.info("info");
+        log.debug("debug");
+        log.trace("trace");
+        Assert.assertEquals(car1, car2);
 
     }
 }
