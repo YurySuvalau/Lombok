7e72492760d8fbdbabe00a250a782b306960ae31
YurySuvalau <yury.suvalau@gmail.com>
2021-02-05 20:49:03 +0300

    Test

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -5,15 +5,25 @@ public class CarTest {
 
     @Test
     public void carTest() {
-Car car1 = Car.builder()
-        .speed(300)
-        .model("M3")
-        .version("diesel")
-        .build();
+        Car car1 = Car.builder()
+                .speed(300)
+                .model("M3")
+                .version("diesel")
+                .build();
+        Car car2 = Car.builder()
+                .speed(500)
+                .model("MTZ")
+                .version("T1000")
+                .build();
+        Car car3 = Car.builder()
+                .speed(58)
+                .model("MTZ")
+                .version("T800")
+                .build();
         System.out.println(car1);
         System.out.println(car2);
         System.out.println(car3);
-        Assert.assertEquals(car1, car2);
+        Assert.assertEquals(car1,car2);
 
     }
 }
